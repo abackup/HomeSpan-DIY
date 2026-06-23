032ed8ad00195327a06aca64682f07b5897ed0a8
abackup <40161397+abackup@users.noreply.github.com>
2026-06-23 12:26:45 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
-#include <HomeSpan.h>
+#include "HomeSpan.h"         // Always start by including the HomeSpan library
 
 void setup() {
 
-homeSpan.begin(Category::Lighting, "MyHomeSpan");
+  Serial.begin(115200);
 
-}
+  homeSpan.begin(Category::Lighting,"HomeSpan LightBulb");   // initializes a HomeSpan device named "HomeSpan Lightbulb" with Category set to Lighting
 
-void loop() {
+  new SpanAccessory();                              // Begin by creating a new Accessory using SpanAccessory(), no arguments needed
+  
+    new Service::AccessoryInformation();            // HAP requires every Accessory to implement an AccessoryInformation Service, with the required Identify Characteristic
+      new Characteristic::Identify();                 // Create the required Identify  
 
-homeSpan.poll();
+    new Service::LightBulb();                       // Create the Light Bulb Service
+      new Characteristic::On();                       // This Service requires the "On" Characterstic to turn the light on and off
+} 
 
-}
+//////////////////////////////////////
+
+void loop(){
+  
+  homeSpan.poll();         // run HomeSpan!
+  
+} 
